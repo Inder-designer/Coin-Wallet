6f61829a78c69e7370fc37ba25662c990a08f27c
Bhupinder <bhupinder7814@gmail.com>
2023-04-28 19:05:39 +0530

    First Commit

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $('.nav_btn').click(function(){
+        $('.side-menu').toggleClass('menu-show')
+        $('body').toggleClass('overflow-hidden')
+    })
+    $('.backdrop').click(function(){
+        $('.side-menu').removeClass('menu-show')
+        $('body').removeClass('overflow-hidden')
+    })
+    $('.close_btn').click(function(){
+        $('.side-menu').removeClass('menu-show')
+        $('body').removeClass('overflow-hidden')
+    })
+    $('.nav-item').click(function(){
+        $('.side-menu').removeClass('menu-show')
+        $('body').removeClass('overflow-hidden')
+    })
+
+    window.addEventListener("scroll",()=>{
+        let scroll = window.scrollY
+        console.log("M",scroll);
+        if(scroll>100){
+          document.getElementById("header")?.classList.add("header-light")
+  
+        }
+        else{
+          document.getElementById("header")?.classList.remove("header-light")
+  
+        }
+    })
+})
